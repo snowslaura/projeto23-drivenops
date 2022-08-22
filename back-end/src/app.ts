527ff55f1213b788f07793af6513c6a4c055ae9d
Laura Neves <lauracmneves@gmail.com>
2022-08-22 15:40:00 -0300

    test cicd

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -8,7 +8,7 @@ app.use(json());
 
 app.get("/students", async (req: Request, res: Response) => {
   const students = await prisma.student.findMany();
-  console.log("Teste cicd");
+  console.log("Teste cicd TESTESTEST");
   
   res.send(students);
 });
