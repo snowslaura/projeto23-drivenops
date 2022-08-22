b266d7b889131545e412ba4a58c07260c68d170d
Laura Neves <lauracmneves@gmail.com>
2022-08-22 15:04:47 -0300

    teste novamente

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -8,7 +8,7 @@ app.use(json());
 
 app.get("/students", async (req: Request, res: Response) => {
   const students = await prisma.student.findMany();
-  console.log("Testes");
+  console.log("Testes testes");
   
   res.send(students);
 });
